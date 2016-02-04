c9a99f30c4a414f216756fcb50ce046ea409caaf
shadowmoon1988 <yingbaolongpku@gmail.com>
2016-02-04 16:50:20 +0800

    Uploading

--- plot2.R
+++ plot2.R
@@ -4,5 +4,5 @@ data[,Date:=as.Date(Date,format="%d/%m/%Y")]
 data <-data[data$Date>=as.Date("2007-02-01")&data$Date<=as.Date("2007-02-02"),]
 t <- strptime(paste(data$Date,data$Time),format="%Y-%m-%d %H:%M:%S")
 png(file="plot2.png",width=480,height=480)
-plot(t,data$Global_active_power,type="l",ylab="Global Active Power(kilowatts)")
+plot(t,data$Global_active_power,type="l",xlab="",ylab="Global Active Power(kilowatts)")
 dev.off()
